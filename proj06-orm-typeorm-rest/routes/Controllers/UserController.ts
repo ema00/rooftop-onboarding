80c43c3cecfb10c5dc25444b5318b13c40e94b8f
Emanuel Aguirre <ema_a_@hotmail.com>
2019-09-26 23:46:01 -0300

    proj06, updated User Controller

--- proj06-orm-typeorm-rest/routes/Controllers/UserController.ts
+++ proj06-orm-typeorm-rest/routes/Controllers/UserController.ts
@@ -9,40 +9,42 @@ export const createUser = async (req, res) => {
 
     try {
         await user.save();
-        res.status(201).json({user: user});
+        res.status(201).json({ user: user });
     }
     catch {
         res.status(500, "DB Error.");
     }
 }
+
 
 export const readUser = async (req, res) => {
     const id = req.body.id;
 
     try {
         const user = await User.findOne(id);
-        res.status(200).json({user: user});
+        res.status(200).json({ user: user });
     }
     catch {
         res.status(500, "DB Error.");
     }
 }
 
+
 export const searchUser = async (req, res) => {
     let search = null;
-    
+
     if (req.body.id) {
-        search = {id: req.body.id};
+        search = { id: req.body.id };
     }
     else if (req.body.nickname) {
-        search = {nickname: req.body.nickname};
+        search = { nickname: req.body.nickname };
     }
     else {
         return res.status(400).json([]);
     }
 
     try {
-        const user = await User.findOne({where: search});
+        const user = await User.findOne({ where: search });
         res.status(200).json([user]);
     }
     catch {
