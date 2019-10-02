81e891d04298acf579f2bf15cda5084ef68edb54
Emanuel Aguirre <ema_a_@hotmail.com>
2019-10-02 00:08:11 -0300

    proj06, refactored Controllers and added comments to HTTP tests

--- proj06-orm-typeorm-http/routes/Controllers/PostController.ts
+++ proj06-orm-typeorm-http/routes/Controllers/PostController.ts
@@ -5,43 +5,66 @@ import { Category } from '../../orm/entity/Category';
 
 export const createPost = async (req, res) => {
     const {title, content, categoryName, userId} = req.body;
+    
+    try {
+        const post = new Post();
+        post.title = title;
+        post.content = content;
 
-    const post = new Post();
-    post.title = title;
-    post.content = content;
+        const user = await User.findOne({ id: userId });
+        if (user.id == userId) {
+            post.craftedBy = user;
+            res.status(201);
+        }
+        else {
+            res.status(404);
+        }
 
-    const user = await User.findOne({ id: userId });
-    if (user.id == userId) {
-        post.craftedBy = user;
-        res.status(201);
-    }
-    else {
-        res.status(404);
-    }
+        let category = await Category.findOne({ name: categoryName });
+        if (category) {
+            post.category = category;
+        }
+        else {
+            category = new Category();
+            category.name = categoryName;
+            await category.save();
+            post.category = category; 
+        }
 
-    let category = await Category.findOne({ name: categoryName });
-    if (category) {
-        post.category = category;
+        await post.save();
+        res.json({post: post});
     }
-    else {
-        category = new Category();
-        category.name = categoryName;
-        await category.save();
-        post.category = category; 
+    catch {
+        res.status(500, "DB Error.");
     }
-
-    await post.save();
-    res.json({post: post});
 }
 
 
-export const searchPostsForUser = async (req, res) => {
+export const searchPosts = async (req, res) => {
+    try {
+        if (req.body.id) {
+            await searchPostsForUser(req, res);
+        }
+        else if (req.body.categoryName) {
+            await searchForPostsByCategory(req, res);
+        }
+        else {
+            res.status(400);
+        }
+    }
+    catch {
+        res.status(500, "DB Error.");
+    }
+}
+
+
+const searchPostsForUser = async (req, res) => {
     const id = req.body.id;
     const categoryName = req.body.categoryName;
-
-    let posts = [];
+    
     let user = (await User.find({ where: { id: id } }))[0];
-
+    let posts = [];
+    
     if (user) {
         if (categoryName) {
             let category = await Category.findOne({ where: { name: categoryName } });
@@ -50,15 +73,22 @@ export const searchPostsForUser = async (req, res) => {
         else {
             posts = await Post.find({ where: { user: user } });
         }
-        res.status(200);
-    }
-    else if (categoryName) {
-        let category = await Category.findOne({ where: { name: categoryName } });
-        posts = await Post.find({ where: { category: category } });
-        category ? res.status(200) : res.status(404);
-    }
+            res.status(200);
+        }
     else {
         res.status(404);
     }
+    res.json(posts);
+}
+
+
+const searchForPostsByCategory = async (req, res) => {
+    const categoryName = req.body.categoryName;
+    
+    let posts = [];
+    
+    let category = await Category.findOne({ where: { name: categoryName } });
+    posts = await Post.find({ where: { category: category } });
+    category ? res.status(200) : res.status(404);
     res.json(posts);
 }
--- proj06-orm-typeorm-http/routes/apiv1.ts
+++ proj06-orm-typeorm-http/routes/apiv1.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { createPost, searchPostsForUser } from './Controllers/PostController';
+import { createPost, searchPosts } from './Controllers/PostController';
 import { createUser } from './Controllers/UserController';
 import { searchUser } from './Controllers/UserController';
 import { searchPostContent } from './Controllers/PostContentController';
@@ -10,7 +10,7 @@ const router = Router();
 router.post('/users', createUser);
 router.get('/users', searchUser);
 router.post('/posts', createPost);
-router.get('/posts', searchPostsForUser);
+router.get('/posts', searchPosts);
 router.get('/content', searchPostContent);
 router.get('/', (req, res) => {
     res.end('API Entry Point.');
