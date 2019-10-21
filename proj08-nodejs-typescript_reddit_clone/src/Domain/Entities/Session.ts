e942c65445c9b915915afd075c66f11156cac5a8
Emanuel Aguirre <ema_a_@hotmail.com>
2019-10-21 20:39:52 -0300

    proj08, fixed Session, now it works

--- proj08-nodejs-typescript_reddit_clone/src/Domain/Entities/Session.ts
+++ proj08-nodejs-typescript_reddit_clone/src/Domain/Entities/Session.ts
@@ -1,4 +1,4 @@
-import { Entity, BaseEntity, PrimaryGeneratedColumn, Column, PrimaryColumn } from "typeorm";
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from "typeorm";
 import { OneToOne } from "typeorm";
 import User from "./User";
 
@@ -6,36 +6,30 @@ import User from "./User";
 @Entity()
 class Session extends BaseEntity {
 
-    private readonly NULL_TOKEN = "";
+    private _id: number;
 
-
-    private _user: User;
+    private _userId: number;
 
     private _token: string;
 
 
-    public constructor(user: User, token: string) {
+    public constructor(userId: number, token: string) {
         super();
-        this._user = user;
-        this._token = token ? token : this.NULL_TOKEN;
+        this._userId = userId;
+        this._token = token;
     }
 
 
-    @PrimaryColumn()
-    @OneToOne(type => User)
-    public get user(): User { return this._user; }
+    @PrimaryGeneratedColumn()
+    public get id(): number { return this._id; }
+    public set id(value: number) { this._id = value; }
 
-    @Column({ nullable: false, default: "" })
+    @OneToOne(type => User)
+    public get userId(): number { return this._userId; }
+
+    @Column({ nullable: false })
     public get token(): string { return this._token; }
     public set token(value: string) { this._token = value; }
-
-    public isValid(): boolean {
-        return this.token.valueOf() != this.NULL_TOKEN.valueOf();
-    }
-
-    public invalidateToken() {
-        this.token = this.NULL_TOKEN;
-    }
 
 }
 
