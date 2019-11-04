bba30df7f31e928b1ea876b490800af2a31497f0
Emanuel Aguirre <ema_a_@hotmail.com>
2019-11-04 20:03:59 -0300

    proj08, in PostController, added paginated results to search

--- proj08-nodejs-typescript_reddit_clone/src/Infrastructure/Controllers/PostController.ts
+++ proj08-nodejs-typescript_reddit_clone/src/Infrastructure/Controllers/PostController.ts
@@ -26,16 +26,29 @@ export class PostController {
     }
 
     public async search(req: Request, res: Response) {
+        const MAX_PAGE_SIZE = 50;
         const { userId, title, content } = req.body;
+        let size = Number(req.body.size);
+        let page = Number(req.body.page);
 
         try {
-            let predicate: any = { };
+            const predicate: any = { };
             if (userId) { predicate.user = { id: userId }; }
             if (title) { predicate.title = Like(`%${title}%`); }
             if (content) { predicate.content = Like(`%${content}%`); }
 
-            let posts = await Post.find({ where: predicate });
-            res.status(200).json(posts);
+            if (size !== undefined && page !== undefined && !isNaN(size) && !isNaN(page)) {
+                size = (0 < size && size <= MAX_PAGE_SIZE) ? size : MAX_PAGE_SIZE;
+                page = (0 <= page) ? page : 0;
+            }
+            else {
+                size = MAX_PAGE_SIZE;
+                page = 0;
+            }
+
+            const [posts, count] =
+                await Post.findAndCount({ where: predicate, take: size, skip: size * page });
+            res.status(200).json({ posts: posts, size: size, page: page, total: count });
         }
         catch (error) {
             res.status(500).json(error);
